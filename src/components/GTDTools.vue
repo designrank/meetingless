066a74860240346605e6cada15336ae86633e381
Boris Turchik <hydralien@gmail.com>
2022-11-19 17:25:14 +0100

    More tools in toolset

--- src/components/GTDTools.vue
+++ src/components/GTDTools.vue
@@ -4,10 +4,16 @@
       Async collaborative tools:
       <a href="https://docs.google.com/" target="_blank">Google Docs</a>,
       <a href="https://figma.com/" target="_blank">Figma</a>
+      <a href="https://miro.com/" target="_blank">Miro</a>
     </li>
     <li>
       Show your thinking with
       <a href="https://loom.com/" target="_blank">Loom</a>
+    </li>
+    <li class="list-unstyled">
+      Communicate for free:
+      <a href="https://slack.com/" target="_blank">Slack</a>,
+      <a href="https://www.microsoft.com/en-us/microsoft-teams/group-chat-software" target="_blank">Microsoft Teams</a>
     </li>
   </ul>
 </template>
